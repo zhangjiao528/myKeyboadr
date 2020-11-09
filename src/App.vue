711b78960641b3c5de848ae76bcdeed075f3b50e
zhangjiao <13060407912@163.com>
2020-11-09 10:51:10 +0800

    项目初始化

---
+++ src/App.vue
@@ -0,0 +1,52 @@
+<template>
+  <div id="app">
+    邮箱地址：<input type="text" v-model="keyboards">
+    <MyKeyboards @getKeyValue="updateKeyValue"/>
+  </div>
+</template>
+
+<script>
+import MyKeyboards from './components/MyKeyboards.vue'
+
+export default {
+  name: 'App',
+  components: {
+    MyKeyboards
+  },
+  data () {
+    return {
+      keyboards: ''
+    }
+  },
+  created () {
+    var a = []
+    for (let i = 0; i < 10; i++) {
+      a[i] = function () {
+        console.log(i)
+      }
+    }
+    a[8]()
+  },
+  methods: {
+    updateKeyValue (keyValue) {
+      this.keyboards = keyValue.toLowerCase()
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+#app input{
+  width: 200px;
+  height: 40px;
+  margin-bottom: 50px;
+}
+</style>
---
+++ src/components/MyKeyboards.vue
@@ -0,0 +1,80 @@
+<template>
+<div class="keybox">
+  <ul class='keyboards'>
+    <li v-for='(item, index) in keyLists' :key='index' :class="{'empty': !item}" @click="clickKey(item)">
+      <span class="iconfont icon-shanchu" v-if="item === 'delete'"></span>
+      <span v-else>{{ item }}</span>
+    </li>
+  </ul>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'MyKeyboards',
+  data () {
+    return {
+      keyValue: '',
+      keyLists: ['', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', 'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', '', '', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'delete', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', '@', '.', '_', '']
+    }
+  },
+  methods: {
+    clickKey (key) {
+      switch (key) {
+        case 'delete': {
+          const kbt = this.keyValue
+          this.keyValue = kbt.length ? kbt.substring(0, kbt.length - 1) : kbt
+          break
+        }
+        default: {
+          this.keyValue += key
+          break
+        }
+      }
+      this.$emit('getKeyValue', this.keyValue)
+    }
+  }
+}
+</script>
+
+<style scoped>
+.keybox{
+  width: 1080px;
+  height: 300px;
+  margin: 0 auto;
+  padding: 17px 26px;
+  border: 1px solid #000;
+  box-sizing: border-box;
+}
+.keyboards{
+  display: flex;
+  flex-wrap: wrap;
+  width: 1028px;
+  height: 266px;
+  margin: 0 auto;
+  padding: 0;
+  list-style: none;
+  border: 1px solid #000;
+  box-sizing: border-box;
+}
+.keyboards li{
+  margin: 0 20px 15px 0;
+  width: 80px;
+  height: 55px;
+  line-height: 50px;
+  text-align: center;
+  border: 2px solid #D2D2D2;
+  border-radius: 10px;
+  color: #333;
+  font-family: PingFang SC;
+  font-size: 22px;
+  font-weight: bold;
+  box-sizing: border-box;
+  cursor: pointer;
+}
+.keyboards li.empty{
+  width: 6px;
+  height: 55px;
+  border: 0;
+}
+</style>
